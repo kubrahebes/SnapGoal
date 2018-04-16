a6b7bf44adeb86f3950a2477e9c26bd0d3ba738c
kubrahebes <kubra.hebes@gmail.com>
2018-04-16 23:44:31 +0300

    sona cok az kala

--- app/src/main/java/com/example/android/snapgoal/MainActivity.java
+++ app/src/main/java/com/example/android/snapgoal/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,14 @@ import com.example.android.snapgoal.fragment.LeaguesFragment;
 import com.example.android.snapgoal.fragment.PostFragment;
 import com.example.android.snapgoal.fragment.SnapMeFragment;
 import com.example.android.snapgoal.fragment.VideoFragment;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     Button setBtn;
     @BindView(R.id.tabbar)
     TabLayout tabbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private TextView mTextMessage;
     Fragment fragment;
@@ -105,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
         postTab = findViewById(R.id.postTab);
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(mSectionsPagerAdapter);
+        tabbar.setSelectedTabIndicatorHeight(0);
         tabbar.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
         setupTabIcons();
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -139,6 +151,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        navigationDrawer();
+
     }
 
     private void setupTabIcons() {
@@ -148,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
         tabbar.getTabAt(3).setIcon(tabIcons[3]);
 
     }
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -200,6 +215,74 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent1 = new Intent(MainActivity.this, SettingActivity.class);
                 startActivity(intent1);
         }
+    }
+
+    public void navigationDrawer() {
+
+
+        //PrimaryDrawerItem primaryDrawerItem = new PrimaryDrawerItem().withIdentifier( 1 ).withName( "test" );
+        SecondaryDrawerItem item1 = new SecondaryDrawerItem().withIdentifier(1)
+                .withName("Home").withIcon(R.drawable.ic_home);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2)
+                .withName("Profile").withIcon(R.drawable.profile);
+        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(3)
+                .withName("Contact Us").withIcon(R.drawable.ic_contact_me);
+        SecondaryDrawerItem item4 = new SecondaryDrawerItem().withIdentifier(4)
+                .withName("Rate App").withIcon(R.drawable.ic_star);
+
+        SecondaryDrawerItem item5 = new SecondaryDrawerItem().withIdentifier(6)
+                .withName("sign out").withIcon(R.drawable.ic_sign_out_option);
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.color.colorPrimaryDark)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Ayse ").withEmail(
+                                "Ayse@gmail.com").withIcon(getResources().getDrawable(R.drawable.avatars_svg
+                        ))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(item1, item2, item3, item4, item5)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        switch (position) {
+
+                            case 1:
+
+                                break;
+                            case 2:
+
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+
+                                break;
+                            case 5:
+
+                                break;
+                            case 6:
+
+                                break;
+                        }
+
+                        return true;
+                    }
+                }).build();
+
+
     }
 
 
